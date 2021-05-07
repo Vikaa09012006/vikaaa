c9710530a632abf3e131fa9cacb6cf6662eaa19a
Vikaa09012006 <74060603+Vikaa09012006@users.noreply.github.com>
2021-05-07 17:08:26 +0300

    Add files via upload

---
+++ lesson42/main.js
@@ -0,0 +1,10 @@
+let a = document.getElementById("test3")
+//   b = a.innerHTML
+//   a.innerHTML ="Здесь был параграф 3"
+// alert(b)
+// a.innerHTML ="Здесь был параграф 3"
+// console.log(a.innerHTML);
+
+// let aDom = document.getElementsByClassName("dom")
+// console.log(aDom);
+a.setAttribute("class","dom")
