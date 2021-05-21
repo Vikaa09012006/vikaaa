1feb81ca0dc6bf13136f041f49574c1c43b86d2c
Vikaa09012006 <74060603+Vikaa09012006@users.noreply.github.com>
2021-05-22 00:39:35 +0300

    Add files via upload

---
+++ lesson39/main.js
@@ -0,0 +1,68 @@
+1. // let a = +prompt("Введите число")
+// switch (a) {
+//     case 0:
+//         alert("Верно"); 
+//     break;
+//     default:
+//         alert("Неверно");
+// }
+
+2. // let a = +prompt("Введите число")
+// switch (a>0) {
+//     case true: 
+//     alert("Верно");
+//     default:
+//         alert("Неверно");
+// }
+
+3. // let a = +prompt("Введите число")
+// switch (a<0) {
+//     case true:
+//         alert("Верно");
+//      default:
+//         alert("Неверно");
+// }
+
+4. // let a = +prompt("Введите число")
+// switch (a>=0) {
+//     case true:
+//         alert("Верно");
+//     default:
+//         alert("Неверно");
+// }
+
+5. // let a = +prompt("Введите число")
+// switch (a<=0) {
+//     case true:
+//         alert("Верно");
+//     default:
+//         alert("Неверно");
+// }
+
+6. // let a = +prompt("Введите число")
+// switch (a != 0) {
+//     case true:
+//         alert("Верно");
+//     default:
+//         alert("Неверно");
+// }
+
+7. // let a = prompt("Введите число")
+// switch (a == "test") {
+//     case true:
+//         alert("Верно");
+//     default:
+//         alert("Неверно");
+// }
+
+8. // let a = prompt("Введите число")
+// switch (a) {
+//     case "1":
+//         alert("Верно");
+//         break;
+//     case '"1"':
+//         alert("Верно");
+//         break;
+//     default:
+//         alert("Неверно");
+// }
