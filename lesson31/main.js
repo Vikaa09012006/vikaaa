4ff667386e3f60e96e125db8314bef6a2acc811b
Vikaa09012006 <74060603+Vikaa09012006@users.noreply.github.com>
2021-05-22 00:34:11 +0300

    Add files via upload

---
+++ lesson31/main.js
@@ -0,0 +1,20 @@
+let x=4
+    y=5
+    Z= x + y
+    x=prompt("x=")
+    y=prompt("y=")
+    alert(x+y)
+//    x++
+//     x--
+//     x+=7
+//     y-=8
+//     y/=4 
+    console.log(x<y);
+    console.log(x+y);
+    console.log(x-y);
+    console.log(x*y);
+    console.log(x/y);
+    console.log(x**y);
+    console.log(y%x);
+    console.log(x);
+    console.log(x==y);
